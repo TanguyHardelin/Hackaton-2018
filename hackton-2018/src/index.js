68f0b9b5d2bdff75f8789884cc1e95a612221ff1
TanguyHardelin <t4hardel@enib.fr>
2018-09-28 22:19:20 -0400

    coucou 3

--- hackton-2018/src/index.js
+++ hackton-2018/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './bootstrap.min.css';
 
 
 import Main from './Main'
